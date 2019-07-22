12fd66d8f3c2ca4370552dba9898ee10383e5e15
onurteber <teberonur00@gmail.com>
2019-07-22 09:19:57 +0300

    initial commit

---
+++ src/main/java/com/example/sistas/SistasSpringRestApiApplication.java
@@ -0,0 +1,26 @@
+package com.example.sistas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.convert.threeten.Jsr310JpaConverters;
+
+import javax.annotation.PostConstruct;
+import java.util.TimeZone;
+
+@SpringBootApplication
+@EntityScan(basePackageClasses = {
+		SistasSpringRestApiApplication.class,
+		Jsr310JpaConverters.class
+})
+public class SistasSpringRestApiApplication {
+
+	@PostConstruct
+	void init() {
+		TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistasSpringRestApiApplication.class, args);
+	}
+}
---
+++ src/main/java/com/example/sistas/repository/RoleRepository.java
@@ -0,0 +1,14 @@
+package com.example.sistas.repository;
+
+import com.example.sistas.model.Role;
+import com.example.sistas.model.RoleName;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+
+@Repository
+public interface RoleRepository extends JpaRepository<Role, Long> {
+    Optional<Role> findByName(RoleName roleName);
+}
